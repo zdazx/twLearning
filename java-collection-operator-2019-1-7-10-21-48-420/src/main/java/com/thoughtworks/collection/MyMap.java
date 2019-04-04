458a6055e0fa4b7f1d97ba45f74ca22bd35ef5d8
zdazx <zhangduo0227@163.com>
2019-04-04 15:19:47 +0800

    commit of MyMap document

--- java-collection-operator-2019-1-7-10-21-48-420/src/main/java/com/thoughtworks/collection/MyMap.java
+++ java-collection-operator-2019-1-7-10-21-48-420/src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,9 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +19,41 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream().map(i -> i*3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream().map(i -> letters[i - 1]).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return array.stream().map(integer ->
+                {
+                    String s = "";
+                    if(integer <= 26){
+                        s = letters[integer - 1];
+                    }else {
+                        int repeat = integer / 26;
+                        if (integer % 26 != 0){
+                            s += letters[repeat - 1];
+                            s += letters[integer % 26 -1];
+                        }else{
+                            s += letters[repeat - 2];
+                            s += "z";
+                        }
+                    }
+                    return s;
+                }
+        ).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        array.sort(Comparator.comparingInt(i -> -i));
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        array.sort(Comparator.comparingInt(i -> i));
+        return array;
     }
 }
